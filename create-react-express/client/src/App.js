5abb7985df9ededa001e5c21343b0a6e4c0e6887
Nicholas Loperena <nicholasloperena@gmail.com>
2019-08-06 18:52:57 -0400

    added a sign up component

--- create-react-express/client/src/App.js
+++ create-react-express/client/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import Header from "./components/Header";
 import Login from "./components/Login";
+import Signup from "./components/Signup"
 // import Awesomebutton from "./components/Awesomebutton";
 import "./App.css";
 
@@ -11,6 +12,7 @@ class App extends Component {
       <div className="App">
         <Header />
         <Login />
+        <Signup />
         {/* <Awesomebutton /> */}
 
 
--- create-react-express/client/src/components/Signup/Signup.js
+++ create-react-express/client/src/components/Signup/Signup.js
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./Login.css";
+import "./Signup.css";
 import loginImg from "../../media/Icons/loginicon.png"
 
-export class Login extends React.Component {
+export class Signup extends React.Component {
 
     constructor(props) {
         super(props)
@@ -22,6 +22,7 @@ export class Login extends React.Component {
                         <div className = "form">
                         <div className = "form-group">
                             <div className = "container">
+                                <p className ="white-text">New here? Sign up!</p>
                         <label className ="white-text" htmlFor="username">Username</label>
                         <input type ="text" name="username" placeholder ="username"></input>
                         </div>
@@ -34,11 +35,18 @@ export class Login extends React.Component {
                         </div>
                         </div>
                         </div>
+                    <div className = "container">
+                        <div className = "form">
+                        <div className = "form-group">
+                        <label className ="white-text" htmlFor="password">Confirm Password</label>
+                        <input className ="border-color" type ="text" name="password" placeholder ="password" ></input>
+                        </div>
+                        </div>
+                        </div>
                     </div>
                 </div>
             </div>
         )};
-
 }
 
-export default Login;
+export default Signup;
