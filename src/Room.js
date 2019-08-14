b83f74d7f5e5cad09c2b3b86fc269b1cc34dcc32
Delane Chen <delane4.0@gmail.com>
2019-08-13 22:28:17 -0400

    swap

--- src/Room.js
+++ src/Room.js
@@ -1,26 +1,67 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+const app = require('express')();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+const index = require("/index.js")
+app.use(index)
+//var liveRooms = [];
 
-function room() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Room/roomId
-        </a>
-      </header>
-    </div>
-  );
-}
+app.get('/', function(req, res){
+    //res.sendFile(__dirname + '/index.html');
 
-export default room;
+
+io.on('connection', function(socket){
+    console.log(socket.id+' user connected');
+    
+
+    //liveRooms.push()
+    socket.on('disconnect', function(){
+        console.log(socket.id+' user diconnected');
+
+    });
+});
+
+});
+
+app.get('/host', function(req, res){
+//res.sendFile(__dirname+ '/host.html');
+    io.on('connection', function(socket){
+        socket.on('howdy',function(data){
+            console.log("host:"+data)
+            io.emit('howdy',data);
+        })
+        console.log(socket.id+" host connected");
+        // let newRoom = new Room(socket.id);
+        // liveRooms.push(newRoom);
+        //console.log(liveRooms);
+        socket.on('disconnect', function(){
+            console.log(socket.id+" host disconnected");
+            // liveRooms = liveRooms.filter(function(liveRooms){
+            //     return liveRooms.pin != socket.id;
+            // });
+            // console.log(liveRooms);
+        });
+    });
+});
+
+
+app.get('/remote', function(req,res){
+//res.sendFile(__dirname+'/remote.html');
+    io.on('connection', function(socket){
+        console.log(socket.id,' remote connected');
+
+        socket.on('howdy',function(data){
+            console.log(data)
+
+            io.to('${'+data.roomid+'}').emit('howdy',data);
+        })
+
+        socket.on('disconnect',function(){
+            console.log(socket.id," remote disconnected");
+        })
+    })
+});
+
+
+http.listen(6800, function(){
+    console.log('listening on *:6800');
+});
